ade02095815635e1278f616eef102d8df1226856
huylamuit <20521389@gm.uit.edu.vn>
2023-10-05 15:45:42 +0700

    song card first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SongCard />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import SongCard from './components/SongCard.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+    SongCard
   }
 }
 </script>
@@ -23,6 +24,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/SongCard.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class ="card">
+        <div class="card_header">
+            <div class="card_image">
+                <div class="play_icon_cover">
+                    <font-awesome-icon class="play_icon" icon="fa-solid fa-user-secret" />
+                </div>
+            </div>
+            
+           
+        </div>
+        <div class="card_info">
+            <p>hehe</p>
+            <p>hehe</p>
+        </div>
+       
+          
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SongCard',
+
+}
+
+
+</script>
+
+<style>
+.card{
+    height: 150px;
+    width:130px;
+
+    border-radius: 16px;
+    padding: 8px;
+    box-sizing: content-box;
+    
+}
+.card_header{
+    background-color: blue;
+    border-radius: 12px;
+    height: 80%;
+    width:80%;
+    margin: 0 auto;
+    overflow: hidden;
+}
+
+.card_image{    
+    position: relative;
+    background-color: blue;
+    border-radius: 5px;
+    height: 100%;
+    width:100%;
+    background: url(https://source.unsplash.com/y3aP9oo9Pjc/500x500);
+    background-size: cover;
+    background-repeat: no-repeat;
+    transition: all ease-in-out .4s;
+    z-index: 0;
+}
+.play_icon_cover{
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    top:50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    z-index: 99;
+    color: white;
+    display: none;
+    background-color: gray;
+    border-radius: 5px;
+
+}
+.play_icon{
+    
+}
+.card:hover .card_image{
+  transform: scale(1.2);
+
+}
+.card:hover .play_icon_cover{
+    display: block;
+}
+
+
+</style>
