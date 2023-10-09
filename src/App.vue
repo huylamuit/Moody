ef7ac1d78dbf2e73064c7954a16049eba06114c8
huylamuit <20521389@gm.uit.edu.vn>
2023-10-09 12:54:48 +0700

    play bar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <SongCard />
-
+    <PlayBar/>
   </div>
 </template>
 
 <script>
 
 import SongCard from './components/SongCard.vue'
+import PlayBar from './components/PlayBar.vue'
 export default {
   name: 'App',
   components: {
-
+    PlayBar,
     SongCard
   }
 }
@@ -24,6 +25,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 1000px;
+  background-color: rgb(36, 36, 36);
+  margin: 0;
+  padding: 0;
+  
 
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,10 +9,23 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faBackwardStep } from '@fortawesome/free-solid-svg-icons'
+import { faForwardStep } from '@fortawesome/free-solid-svg-icons'
+import { faPause } from '@fortawesome/free-solid-svg-icons'
+import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons'
+import { faVolumeLow } from '@fortawesome/free-solid-svg-icons'
+import { faVolumeMute } from '@fortawesome/free-solid-svg-icons'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
 library.add(faUserSecret)
-
+library.add(faBackwardStep)
+library.add(faForwardStep)
+library.add(faPause)
+library.add(faPlay)
+library.add(faVolumeHigh)
+library.add(faVolumeLow)
+library.add(faVolumeMute)
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
